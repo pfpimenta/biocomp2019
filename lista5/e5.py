add2073d78e344ff84e81bea83bc65f453e15cfa
pfpimenta <pedrofolettopimenta@gmail.com>
2019-12-04 21:41:34 -0300

    comentarios, organizacao

--- lista5/e5.py
+++ lista5/e5.py
@@ -16,10 +16,12 @@ import time
 #######################################################
 ## funcoes
 
-#?                  # retorna a string consenso entre as sequencias (sequences)
-#?                  # de tamanho (motif_size) e o score do consenso
+# dadas as sequencias (array de strings) (sequences)
+# e o tamanho do padrao (motif_lenght)
+# retorna
+# a string consenso (consensus_string)
+# e o score do consenso (score)
 def find_motif(sequences, motif_lenght):
-    pass # TODO
     num_sequences = len(sequences)
 
     # get start positions que maximizam o score
@@ -32,7 +34,13 @@ def find_motif(sequences, motif_lenght):
     
     return consensus_string, score
 
-
+# dadas
+# as sequencias (sequences),
+# posicoes iniciais do motif (start_positions)
+# e seu tamanho (motif_lenght)
+# retorna
+# a string consenso (consensus_string)
+# e o score do consenso (score)
 def consensus(sequences, start_positions, motif_lenght):
     num_sequences = len(sequences)
     assert(num_sequences == len(start_positions))
@@ -48,10 +56,14 @@ def consensus(sequences, start_positions, motif_lenght):
     pfm = get_pfm(sequences) # position frequency matrix
     print(pfm) # DEBUG
     consensus_string = get_consensus_string(pfm)
+    
+    # get score
+    score = 10 # TODO
 
-    return consensus_string, 10
+    return consensus_string, score
 
-
+# given an array of sequences (array of strings)
+# returns the pfm (pattern frequency matrix)
 def get_pfm(sequences):
     num_sequences = len(sequences)
     motif_lenght = len(sequences[0])
@@ -74,21 +86,24 @@ def get_pfm(sequences):
                 print("ihhh deu merda")
     return pfm
 
+# given a pattern frequency matrix
+# returns the consensus string
 def get_consensus_string(pfm):
     _, motif_lenght = np.shape(pfm)
     consensus_string = ''  # init 
     for i in range(len(pfm)):
-            coco = np.argmax(pfm[:, i]) # TODO DEBUG mudar nome da var
-            if(coco == 0): # a
+            letra = np.argmax(pfm[:, i]) # acha letra do consenso na posicao i
+            # TODO : mmudar esse if else feio pra algo bunitu
+            if(letra == 0): # a
                 consensus_string += 'a'
-            elif(coco == 1): # c
+            elif(letra == 1): # c
                 consensus_string += 'c'
-            elif(coco == 2): # g
+            elif(letra == 2): # g
                 consensus_string += 'g'
-            elif(coco == 3): # t
+            elif(letra == 3): # t
                 consensus_string += 't'
             else:
-                print("ihhh deu merda")
+                print("ihhh deu merda") # nunca chega aqui
 
     return consensus_string
 
@@ -118,30 +133,41 @@ print("rodando algoritmo de consensus...")
 startTime = time.time() # medir o tempo de execucao a partir daqui
 
 
+# DEBUG TEST
+# testando o algoritmo consensus "basico"
+# tu da sequences, starting_positions, e  
+motif_lenght = 4
+consensus_string, score = find_motif(sequences_1, motif_lenght)
+# DEBUG:
+print("\n\n...resultados:")
+print("consensus: " + str(consensus_string))
+print("score: " + str(score))
+
 
 ## exercicio a:
 
 # Encontrar o motivo de tamanho 8 aceitando 2 mutações;
 motif_lenght = 8
-consensus_string, score = find_motif(sequences_1, motif_lenght)
-# DEBUG:
-print("\n\n...resultados:")
-print(consensus_string)
-print(score)
+mutations_accepted = 2
+#consensus_string, score = motif_finder()
 
 # Encontrar o motivo de tamanho 5 aceitando 3 mutações;
 motif_lenght = 5
+mutations_accepted = 3
 # consensus_string, score = consensus(sequences_1, )
 
 # Encontrar o motivo de tamanho 3 aceitando 1 mutação;
 motif_lenght = 3
+mutations_accepted = 1
 # consensus_string, score = consensus(sequences_1, )
 
 ## exercicio b:
 # Encontrar o motivo de tamanho 3 aceitando 1 mutações;
 motif_lenght = 3
+mutations_accepted = 1
 # Encontrar o motivo de tamanho 5 aceitando 2 mutações;
 motif_lenght = 5
+mutations_accepted = 2
 
 endTime = time.time()
 totalTime = endTime - startTime
